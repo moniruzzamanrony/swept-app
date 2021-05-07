688047281b68d60c4f23b10d0dbd1e747a7b0a13
moniruzzaman rony <moniruzzaman35-223@diu.edu.bd>
2021-05-07 23:20:56 +0600

    -- Added Profile Update Option

--- src/screens/Dashboard/MyProfileScreen.js
+++ src/screens/Dashboard/MyProfileScreen.js
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Image, StyleSheet, TextInput, View } from "react-native";
+import { AsyncStorageStatic as AsyncStorage, Image, StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
 import NavigationBar from "../../navigation/NavigationBar";
 import { Text } from "react-native-elements";
 import { colors } from "../../theme/Colors";
 import axios from "axios";
 import { Api } from "../../contants/Api";
-import { Root } from "native-base";
+import { Button, Icon, Root } from "native-base";
 import Spinner from "react-native-loading-spinner-overlay";
 
-const MyProfileScreen = () => {
+const MyProfileScreen = (props) => {
   const [name, setName] = React.useState("");
   const [nameDisable, setNameDisable] = React.useState(false);
   const [email, setEmail] = React.useState("");
   const [emailDisable, setEmailDisable] = React.useState(false);
   const [phone, setPhone] = React.useState("");
-  const [phoneDisable, Err, setPhoneDisable] = React.useState(false);
+  const [phoneDisable, setPhoneDisable] = React.useState(false);
   const [address, setAddress] = React.useState("");
   const [addressDisable, setAddressDisable] = React.useState(false);
   const [loading, setLoading] = useState(false);
+  const [isEditButVisible, setEditButVisible] = useState(true);
 
 
   useEffect(() => {
@@ -30,9 +31,12 @@ const MyProfileScreen = () => {
     setLoading(true);
     axios.get(Api.BASE_URL + Api.GET_PROFILE)
       .then(function(response) {
+        setName(response.data.user.name);
+        setEmail(response.data.user.email);
+        setPhone(response.data.user.phone);
+        setAddress(response.data.user.address);
 
-        console.log(response.data);
-
+        console.log(name);
         // Hide Loader
         setLoading(false);
       })
@@ -42,7 +46,40 @@ const MyProfileScreen = () => {
         setLoading(false);
       });
   };
+  const logout = () => {
+    removeToken("token");
+    props.navigation.navigate("LoginScreen");
+  };
 
+  const editProfile = () => {
+    setNameDisable(true);
+    setEmailDisable(true);
+    setPhoneDisable(true);
+    setAddressDisable(true);
+    setEditButVisible(false);
+
+  };
+  const removeToken = async (key) => {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const updateProfile = () => {
+    setNameDisable(false);
+    setEmailDisable(false);
+    setPhoneDisable(false);
+    setAddressDisable(false);
+    setEditButVisible(true);
+  };
+
+  const uploadProfileImage = () => {
+    // ImagePicker.openPicker().then(image => {
+    //   console.log(image);
+    // });
+  };
 
   return (
     <Root>
@@ -57,39 +94,75 @@ const MyProfileScreen = () => {
       <View>
         {/* ---- Header ------*/}
         <NavigationBar title="Profile" url="LoginScreen" />
+        <TouchableOpacity onPress={function() {
+          uploadProfileImage();
+        }}>
+          <View style={{
+            justifyContent: "center",
+            alignItems: "center", margin: 10,
+          }}>
 
-        <View style={{ justifyContent: "center", alignItems: "center", margin: 10 }}>
-          <Image
-            source={require("../../../assets/avatar/profile.png")}
-            style={{ width: 100, height: 100, borderRadius: 100 / 2 }}
-          />
-        </View>
-
+            <Image
+              source={require("../../../assets/avatar/profile.png")}
+              style={{
+                width: 120, height: 120, borderRadius: 120 / 2, borderWidth: 3,
+                borderColor: "#ff8b7e",
+              }}
+            />
+          </View>
+        </TouchableOpacity>
         {/*Input Field*/}
         <View style={style.formDiv}>
           <Text style={{ margin: 2 }}>Name</Text>
           <TextInput style={style.inputField} onChangeText={name => setName(name)}
-                     editable={nameDisable} />
+                     editable={nameDisable} value={name} />
         </View>
 
         <View style={style.formDiv}>
           <Text style={{ margin: 2 }}>Email</Text>
           <TextInput style={style.inputField} onChangeText={phoneOrEmail => setEmail(phoneOrEmail)}
-                     editable={emailDisable} />
+                     editable={emailDisable} value={email} />
         </View>
 
         <View style={style.formDiv}>
           <Text style={{ margin: 2 }}>Phone No.</Text>
           <TextInput style={style.inputField} onChangeText={phone => setPhone(phone)}
-                     editable={phoneDisable} />
+                     editable={phoneDisable} value={phone} />
         </View>
 
         <View style={style.formDiv}>
           <Text style={{ margin: 2 }}>Address</Text>
           <TextInput style={style.inputField} onChangeText={address => setAddress(address)}
-                     editable={addressDisable} />
+                     editable={addressDisable} value={address} />
+        </View>
+
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          {isEditButVisible ?
+            <Button style={style.getStartBut} onPress={function() {
+              editProfile();
+            }}>
+              <Icon name="md-create-outline" style={{ fontSize: 18, color: colors.black }} />
+              <Text style={{ fontSize: 18, fontWeight: "bold", marginRight: 20 }}>Edit</Text>
+            </Button>
+            :
+            <Button style={style.getStartBut} onPress={function() {
+              updateProfile();
+            }}>
+              <Icon name="md-create-outline" style={{ fontSize: 18, color: colors.black }} />
+              <Text style={{ fontSize: 18, fontWeight: "bold", marginRight: 20 }}>Update</Text>
+            </Button>
+          }
+
+
+          <Button style={style.getStartBut} onPress={function() {
+            logout();
+          }}>
+            <Icon name="md-power" style={{ fontSize: 18, color: colors.black }} />
+            <Text style={{ fontSize: 18, fontWeight: "bold", marginRight: 20 }}>Logout</Text>
+          </Button>
         </View>
       </View>
+
     </Root>
   );
 };
@@ -108,13 +181,15 @@ const style = StyleSheet.create({
     borderColor: colors.offWhite,
   },
   getStartBut: {
-    width: 292,
-    height: 60,
+    width: 150,
+    height: 50,
+    margin: 20,
     marginTop: 30,
     backgroundColor: colors.buttonBgColor,
     color: colors.black,
     borderRadius: 9,
     justifyContent: "center",
+    alignItems: "center",
   },
   errorMessage: {
     fontSize: 11,
